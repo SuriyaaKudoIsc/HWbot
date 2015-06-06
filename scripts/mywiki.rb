bda4303fafc5d05ee61e57016f757c4f61f2c9ca
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-06-06 18:40:33 +0200

    Update mywiki.rb

--- scripts/mywiki.rb
+++ scripts/mywiki.rb
@@ -1,4 +1,5 @@
 require 'media_wiki'
 mw = MediaWiki::Gateway.new('http://suriyaakudo.bplaced.net/wiki/api.php')
 mw.login('HWBot', 'pa$$w0rd')
-mw.create('PageTitle', 'Hello world!', summary: 'My first page')
+mw.create('User:HWBot/sandbox', 'Hello world!', summary: 'My first page.')
+
