77f5f972b026acf7e03be91c0b1c4655fadbb06f
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-06-06 18:40:06 +0200

    Update wikipedia.rb

--- scripts/wikipedia.rb
+++ scripts/wikipedia.rb
@@ -1,4 +1,4 @@
 require 'media_wiki'
 mw = MediaWiki::Gateway.new('http://en.wikipedia.org/w/api.php')
-mw.login('hwBot', 'pa$$w0rd')
-mw.create('User:hwBot/sandbox', 'Hello world!', summary: 'My first page')
+mw.login('HWBot', 'pa$$w0rd')
+mw.create('User:HWBot/sandbox', 'Hello world!', summary: 'My first page.')
