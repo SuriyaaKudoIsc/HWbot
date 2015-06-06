e8c61477c8bc69976fe79a0524d01ea58a63316f
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-06-06 18:40:26 +0200

    Update example.rb

--- scripts/example.rb
+++ scripts/example.rb
@@ -1,4 +1,4 @@
 require 'media_wiki'
 mw = MediaWiki::Gateway.new('http://my-wiki.example/w/api.php')
-mw.login('RubyBot', 'pa$$w0rd')
-mw.create('PageTitle', 'Hello world!', summary: 'My first page')
+mw.login('HWBot', 'pa$$w0rd')
+mw.create('User:HWBot/sandbox', 'Hello world!', summary: 'My first page.')
